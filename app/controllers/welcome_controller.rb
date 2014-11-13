5b8d116dc04d2c9b8b67fcad3bb5fa7778f30ed7
Andrea Folds <andreafolds@gmail.com>
2014-11-13 14:10:29 -0500

    FINE

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+  	@items = Item.all.order("do_by")
+
+  	@uniquedates =[]
+
+  	@items.each do |item|
+  		@uniquedates = @uniquedates.push(item.do_by)
+  		
   end
 end
